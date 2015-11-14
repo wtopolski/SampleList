dc6201b859e05b0132aaaf17018cf76430bbdccc
Wojciech Topolski <wojciech.topolski@gmail.com>
2015-11-15 00:29:36 +0100

    Move sample menu to single element fragment

--- app/src/main/java/wtopolski/android/samplelist/ElementSingleFragment.java
+++ app/src/main/java/wtopolski/android/samplelist/ElementSingleFragment.java
@@ -3,6 +3,9 @@ package wtopolski.android.samplelist;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -14,6 +17,12 @@ public class ElementSingleFragment extends Fragment {
     public static final String ARGUMENT_ID = "id";
 
     @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_single, container, false);
 
@@ -21,4 +30,73 @@ public class ElementSingleFragment extends Fragment {
 
         return view;
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        getActivity().getMenuInflater().inflate(R.menu.menu_single, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+/*
+        // Insert test
+        ContentValues values = new ContentValues();
+        values.put(DBContract.ElementTable.TITLE_COLUMN, "title " + (new Date()).toGMTString());
+        values.put(DBContract.ElementTable.DESC_COLUMN, "desc");
+        Uri newUri = getContentResolver().insert(ElementProvider.ELEMENT_URI, values);
+
+        // Query test
+        String[] projection = new String[] {
+                DBContract.ElementTable._ID,
+                DBContract.ElementTable.TITLE_COLUMN,
+                DBContract.ElementTable.DESC_COLUMN};
+        Cursor cursor = getContentResolver().query(ElementProvider.ELEMENT_URI, projection, null, null, null);
+
+        long firstId = -1L;
+        if (cursor.moveToFirst()) {
+            int idColumnIndex = cursor.getColumnIndex(DBContract.ElementTable._ID);
+            int titleColumnIndex = cursor.getColumnIndex(DBContract.ElementTable.TITLE_COLUMN);
+            int descColumnIndex = cursor.getColumnIndex(DBContract.ElementTable.DESC_COLUMN);
+
+            do {
+                if (firstId < 0) {
+                    firstId = cursor.getLong(idColumnIndex);
+                }
+                String title = cursor.getString(titleColumnIndex);
+                String desc = cursor.getString(descColumnIndex);
+                Log.d("wtopolski", "title: " + title + " desc: " + desc);
+            } while (cursor.moveToNext());
+        }
+
+        if (!cursor.isClosed()){
+            cursor.close();
+        }
+
+        // Update test
+        values = new ContentValues();
+        values.put(DBContract.ElementTable.DESC_COLUMN, "desc update");
+        int updateCount = getContentResolver().update(newUri, values, null, null);
+        Log.d("wtopolski", "updateCount: " + updateCount);
+
+        // Delete test
+        Uri deleteUri = ContentUris.withAppendedId(ElementProvider.ELEMENT_URI, firstId);
+        int deleteCount = getContentResolver().delete(deleteUri, null, null);
+        Log.d("wtopolski", "deleteCount: " + deleteCount);
+        */
 }
